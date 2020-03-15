e35945bd6fb2e3aee362ea9d497e79e93b3321f1
jmassing <julimassing@gmail.com>
2020-03-15 10:48:19 +0100

    inital commit for Game.h. Base features only

---
+++ poker/include/Game.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+
+#include "Hand.h"
+#include "Deck.h"
+#include "Card.h"
+#include "Mapping.h"
+
+namespace poker{
+    
+    class Game{
+        private:
+        std::vector<Hand> player_hands;
+        int nr_of_players;
+
+        public:
+        explicit Game(int nr_of_players);
+        ~Game();
+
+        std::vector<Hand> getHands() {return this->player_hands;}    
+
+        // Using default copy and move constructors. 
+        Game(const Game& other) = default;
+        Game& operator=(const Game& other) = default;
+        Game(Game&& other) noexcept = default;
+        Game& operator=(Game&& other) = default;
+    };
+
+}//end namespace poker
---
+++ poker/src/Game.cpp
@@ -0,0 +1,15 @@
+#include "Game.h"
+
+namespace poker{
+
+    Game::Game(int nr_of_players):  nr_of_players{nr_of_players}
+    {
+        // at the start of the game, we don't know any cards
+        Hand empty_hand;
+        for(int i=0; i<nr_of_players;++i)
+        {
+            this->player_hands.emplace_back(empty_hand);
+        }
+    }
+
+}// end namespace poker
