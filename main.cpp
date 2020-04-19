848cc43a7c4289024dfac03b06831a9aaf76e286
jmassing <julimassing@gmail.com>
2020-04-19 15:51:27 +0200

    First combination of Detection, Poker and visualization done.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include "View.h"
 #include "Hand.h"
 #include "Card.h"
+#include "Simulation.h"
 
 
 using namespace cv;
@@ -21,6 +22,19 @@ using namespace std;
 using namespace detect;
 using namespace poker;
 
+template<class T>
+bool contains(Card input_card, T cards)
+{
+	for(const auto& card: cards)
+	{
+		if(card.rank==input_card.rank && card.suit==input_card.suit)
+		{
+			return true;
+		}
+	} 
+	
+	return false;
+}
 
 int main(int argc, char* argv[])
 {
@@ -39,7 +53,10 @@ int main(int argc, char* argv[])
 	cout << "Start grabbing" << endl
 		<< "Press any key to terminate" << endl;
 
+	array<BaseCard,2> robot_cards;
 
+	Simulation sim(5,10000);
+	int nr_of_sim_runs=0;
 	for (;;)
 	{
 		// Grab live frame and check if it worked
@@ -56,21 +73,33 @@ int main(int argc, char* argv[])
 		//				Simulation						//
 		// ************************************************ //
 		vector<Card> known_cards=detect.getCards();
-		array<Card,2> robot_cards;
-		vector<Card> public_cards;
+		vector<BaseCard> public_cards;
 		
 		for(const auto& card: known_cards)
-		{
-			cout << card.rank << card.suit << ": (" << card.center_point.x << ", " << card.center_point.y << ")" << endl;
-		}
-
-
+		{	
+			for(auto&robot_card : robot_cards)
+			{
+				if(card.rank != UNKNOWN && card.suit != UNKNOWN && robot_card.rank == UNKNOWN && card.center_point.x < 1500 && card.center_point.y > 800 && !contains(card,robot_cards))
+				{
+					robot_card=card;
+				}
+				else if(card.rank != UNKNOWN && card.suit != UNKNOWN && card.center_point.x < 1500 && card.center_point.y < 800 && !contains(card,public_cards))
+				{
+					public_cards.emplace_back(card);
+				}
+			}	
+		}		
+				
+		pair<double,double> prob=sim.run(public_cards, robot_cards);
+		++nr_of_sim_runs;
+				
 		// ************************************************ //
 		//				Visualization						//
 		// ************************************************ //
 		
 		View visualize;
 		visualize.drawCards(detect.getCards(), live.frame_, Scalar(0, 255, 0));
+		visualize.printProbability(live.frame_, prob);
 
 		imshow("live",live.frame_);
 
