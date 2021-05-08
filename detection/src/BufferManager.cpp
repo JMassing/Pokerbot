0b0d159ccedbb0233be128651f07fde898565d1a
jmassing <julimassing@gmail.com>
2021-05-08 20:14:47 +0200

    update

--- detection/src/BufferManager.cpp
+++ detection/src/BufferManager.cpp
@@ -3,11 +3,11 @@
 namespace detect{
     void BufferManager::addDetectedCard(const Card& card, const int& detection_frame, const int& max_distance)
     {
-        if(this->buffers_.size() == 0)
+		if(this->buffers_.size() == 0)
 		{
 			this->buffers_.emplace_back(
-                CardBuffer<CARD_BUFFER_SIZE>(card, detection_frame)
-                );
+					CardBuffer<CARD_BUFFER_SIZE>(card, detection_frame)
+				);
 		}
 		else
 		{
@@ -16,11 +16,11 @@ namespace detect{
 			for(const auto& buffer: this->buffers_)
 			{
 				squared_distances.emplace_back(
-                    templates::squaredEuclideanDistance2D(buffer.getCenter(), card.center_point)
-                    );
+					templates::squaredEuclideanDistance2D(buffer.getCenter(), card.center_point)
+				);
 			}
 			// find min distance and add card to that buffer, if the distance is small enough. 
-            // Otherwise it is a new card and a new buffer is created
+			// Otherwise it is a new card and a new buffer is created
 			auto p = std::min_element(squared_distances.begin(), squared_distances.end());
 			auto pos = std::distance(squared_distances.begin(), p);
 			if( *p < max_distance * max_distance)
@@ -30,16 +30,16 @@ namespace detect{
 			else
 			{
 				this->buffers_.emplace_back(
-                    CardBuffer<CARD_BUFFER_SIZE>(card, detection_frame)
-                    );
+					CardBuffer<CARD_BUFFER_SIZE>(card, detection_frame)
+				);
 			}			
 		}
     }
 
     std::vector<Card> BufferManager::getBufferedCards()
     {
-        std::vector<Card> cards;
-        for(auto p = this->buffers_.begin(); p != this->buffers_.end(); ++p)
+		std::vector<Card> cards;
+		for(auto p = this->buffers_.begin(); p != this->buffers_.end(); ++p)
 		{
 			Card card_tmp;
 			// only use buffers that were updated in the last 3 frames
@@ -47,18 +47,18 @@ namespace detect{
 			{				
 				cards.emplace_back(card_tmp);
 			}
-            else
-            {
-                //do nothing
-            }            	
-        }
+			else
+			{
+				//do nothing
+			}            	
+		}
 
-        return cards;
+		return cards;
     }
 
     void BufferManager::removeStaleBuffers(const int& update_threshold, const int& current_frame)
     {
-        for(auto p = this->buffers_.begin(); p != this->buffers_.end(); ++p)
+		for(auto p = this->buffers_.begin(); p != this->buffers_.end(); ++p)
 		{
 			if((*p).getLastUpdate() <= current_frame - update_threshold)
 			{				
@@ -67,7 +67,7 @@ namespace detect{
 			}			
 			else
 			{
-					//do nothing
+				//do nothing
 			}		
 		}	
     }
